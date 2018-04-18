559a95cd518fcd658637bab2b51e07b8a729bbb2
Miguel Echenique <miguelechenique@outlook.com>
2018-04-18 15:46:28 +0200

    update config

--- config/prod.js
+++ config/prod.js
@@ -6,14 +6,14 @@ const config = {
     token_uri: 'https://accounts.google.com/o/oauth2/token',
     auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
     client_secret: '-6REQvBz2Rf7FD_QQViobQFr',
-    redirect_uris: ['http://gpi2unavarra.hopto.org:3000/login/callback'],
-    javascript_origins: ['gpi2unavarra.hopto.org:3000'],
+    redirect_uris: ['https://gymkhana-server.herokuapp.com//login/callback'],
+    javascript_origins: ['gymkhana-server.herokuapp.com/3000'],
     scopes: [
       'https://www.googleapis.com/auth/plus.me',
     ],
   },
   mongodb: {
-    mongo_uri: 'mongodb://localhost:27017/btesoro',
+    mongo_uri: `mongodb+srv://miguel:${process.env.MONGO_PASS}@gymkhana-zdcel.mongodb.net/btesoro`,
     collections: {
       usersCollection: 'users',
     },
